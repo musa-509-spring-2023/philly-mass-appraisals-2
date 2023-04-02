2ed1dc34436226f2f041e1be59a5ffbc06667c00
Yihong Hu <yihonghu1997@gmail.com>
2023-04-01 22:28:29 -0400

    preparing extracting function as a google cloud function

--- tasks/extract_opa.py
+++ tasks/extract_opa.py
@@ -1,17 +1,21 @@
 from urllib.request import urlopen
 import pathlib
+from google.cloud import storage
 
-
-RAW_DATA_DIR = pathlib.Path(__file__).parent/ 'raw_data'
-PROCESSED_DATA_DIR = pathlib.Path(__file__).parent / 'processed_data'
+# RAW_DATA_DIR = pathlib.Path(__file__).parent/ 'raw_data'
+client = storage.Client()
+bucket = client.get_bucket("musa509s23_team2_raw_data")
 
 url = "https://phl.carto.com/api/v2/sql?filename=opa_properties_public&format=geojson&skipfields=cartodb_id&q=SELECT+*+FROM+opa_properties_public"
 filename = 'opa.geojson'
 
 response = urlopen(url)
-with open(RAW_DATA_DIR / filename, 'wb') as f:
-    while True:
-        chunck = response.read(1024 * 1024)
-        if not chunck:
-            break
-        f.write(chunck)
+# with open(RAW_DATA_DIR / filename, 'wb') as f:
+#     while True:
+#         chunck = response.read(1024 * 1024)
+#         if not chunck:
+#             break
+#         f.write(chunck)
+
+blob = bucket.blob('phl/opa.geojson')
+blob.upload_from_string(response, content_type = 'application/geojson')
