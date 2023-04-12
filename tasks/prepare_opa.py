114c71f98a92b19798ec85c93509443986c8ebcf
Yihong Hu <yihonghu1997@gmail.com>
2023-04-12 13:13:22 -0400

    Created a pipeline to convert geojson to jsonl
    We have run the local function that prepare geojson file into jsonl to be used on google cloud.
    
    Currently Jsonl is successfully uploaded. Next step is to create google cloud functions with a trigger.

--- tasks/prepare_opa.py
+++ tasks/prepare_opa.py
@@ -1,14 +1,36 @@
 import json
-import pathlib
+from urllib.request import urlopen
+import io
+from google.cloud import storage
+import dotenv
+dotenv.load_dotenv()
 
 # RAW_DATA_DIR = pathlib.Path(__file__).parent/ 'raw_data'
 # PROCESSED_DATA_DIR = pathlib.Path(__file__).parent / 'processed_data'
+client = storage.Client()
+bucket = client.bucket("musa509s23_team02_raw_data")
 
-with open(RAW_DATA_DIR/'opa.geojson', 'rU', encoding='utf-8') as f:
-    data = json.load(f)
+blob = bucket.blob("opa_properties/opa.geojson")
+content = blob.download_as_string()
 
-with open(PROCESSED_DATA_DIR / 'opa_propeorties.jsonl', 'w') as f:
-    for feature in data['features']:
-        row = feature['properties']
-        row['geog'] = json.dumps(feature['geometry'])
-        f.write(json.dumps(row) + '\n')
+# with open(RAW_DATA_DIR/'opa.geojson', 'rU', encoding='utf-8') as f:
+
+data = json.loads(content)
+
+processed_blob = bucket.blob('opa_properties/opa_processed.jsonl')
+
+jsonl_data = []
+for feature in data['features']:
+    row = feature['properties']
+    row['geog'] = json.dumps(feature['geometry'])
+    jsonl_data.append(json.dumps(row))
+
+processed_blob.upload_from_string('\n'.join(jsonl_data), content_type = 'application/jsonl')
+
+#with open(PROCESSED_DATA_DIR / 'opa_propeorties.jsonl', 'w') as f:
+  #for feature in data['features']:
+  #    row = feature['properties']
+  #    row['geog'] = json.dumps(feature['geometry'])
+  #    f.write(json.dumps(row) + '\n')
+  #
+  #  processed_blob.upload_from_file(
