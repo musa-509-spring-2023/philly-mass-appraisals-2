03c028d6eadb0cfb730a5f3e116673b6a2b7755e
Sofia <98534255+SofiaFasullo@users.noreply.github.com>
2023-04-12 11:26:33 -0400

    Update ui/reviewer/js/main.js
    Co-authored-by: Mjumbe Poe <mjumbewu@gmail.com>

--- ui/reviewer/js/main.js
+++ ui/reviewer/js/main.js
@@ -2,12 +2,6 @@ import { initMap } from './map.js';
 
 console.log("main");
 
-let app = {
-  currentTree: null,
-  notes: null,
-};
-
-const loadOverlayEl = document.getElementById('load-overlay');
 const map = initMap();
 
 window.app = app;
