6a9849d1a49e27d0bbf84ac9b97a9ee97cb0b467
Mjumbe Poe <mjumbewu@gmail.com>
2023-04-25 16:51:56 -0400

    Clean up unused code

--- tasks/extract_opa_cloud/main.py
+++ tasks/extract_opa_cloud/main.py
@@ -4,10 +4,6 @@ import dotenv
 dotenv.load_dotenv()
 import functions_framework
 
-# import os
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/huyh/Documents/Penn/Spring 2023/Cloud Computing/class-project/philly-mass-appraisals-2/key/musa509s23-team2-philly-cama-67593d6af962.json"
-
-# RAW_DATA_DIR = pathlib.Path(__file__).parent/ 'raw_data'
 
 @functions_framework.http
 def extract_opa(request):
@@ -15,17 +11,9 @@ def extract_opa(request):
     bucket = client.bucket("musa509s23_team02_raw_data")
 
     url = "https://phl.carto.com/api/v2/sql?filename=opa_properties_public&format=geojson&skipfields=cartodb_id&q=SELECT+*+FROM+opa_properties_public"
-    filename = 'opa.geojson'
-
     response = urlopen(url)
-    # with open(RAW_DATA_DIR / filename, 'wb') as f:
-    #     while True:
-    #         chunck = response.read(1024 * 1024)
-    #         if not chunck:
-    #             break
-    #         f.write(chunck)
 
     blob = bucket.blob('opa_properties/opa.geojson')
-    blob.upload_from_string(response.read(), content_type = 'application/geojson')
+    blob.upload_from_string(response.read(), content_type='application/geojson')
 
     return 'OK'
