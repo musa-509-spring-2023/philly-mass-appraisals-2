9e7b4b839d950f5fd715e92d37f9255827cbaab7
Mjumbe Poe <mjumbewu@gmail.com>
2023-04-25 16:47:36 -0400

    Convert linear script code for extracting properties to a function; add requirements

--- tasks/extract_opa_cloud/main.py
+++ tasks/extract_opa_cloud/main.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen
 from google.cloud import storage
-import dotenv 
+import dotenv
 dotenv.load_dotenv()
 import functions_framework
 
@@ -9,21 +9,23 @@ import functions_framework
 
 # RAW_DATA_DIR = pathlib.Path(__file__).parent/ 'raw_data'
 
-@functions_framework.http(request):
-def extract_opa
-client = storage.Client()
-bucket = client.bucket("musa509s23_team02_raw_data")
+@functions_framework.http
+def extract_opa(request):
+    client = storage.Client()
+    bucket = client.bucket("musa509s23_team02_raw_data")
 
-url = "https://phl.carto.com/api/v2/sql?filename=opa_properties_public&format=geojson&skipfields=cartodb_id&q=SELECT+*+FROM+opa_properties_public"
-filename = 'opa.geojson'
+    url = "https://phl.carto.com/api/v2/sql?filename=opa_properties_public&format=geojson&skipfields=cartodb_id&q=SELECT+*+FROM+opa_properties_public"
+    filename = 'opa.geojson'
 
-response = urlopen(url)
-# with open(RAW_DATA_DIR / filename, 'wb') as f:
-#     while True:
-#         chunck = response.read(1024 * 1024)
-#         if not chunck:
-#             break
-#         f.write(chunck)
+    response = urlopen(url)
+    # with open(RAW_DATA_DIR / filename, 'wb') as f:
+    #     while True:
+    #         chunck = response.read(1024 * 1024)
+    #         if not chunck:
+    #             break
+    #         f.write(chunck)
 
-blob = bucket.blob('opa_properties/opa.geojson')
-blob.upload_from_string(response.read(), content_type = 'application/geojson')
+    blob = bucket.blob('opa_properties/opa.geojson')
+    blob.upload_from_string(response.read(), content_type = 'application/geojson')
+
+    return 'OK'
