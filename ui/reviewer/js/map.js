a57ec98d7d228bf70eb657879a79f4629cd45485
Sofia <sfasull1@binghamton.edu>
2023-04-26 13:16:29 -0400

    so close to getting conditional formatting
    Added functions to map.js to create a 4 styling colors based on quadrants based on current_value, but seeing if all the different fields can be compared to that standard.

--- ui/reviewer/js/map.js
+++ ui/reviewer/js/map.js
@@ -1,3 +1,76 @@
+const colors = ["#f0bc1f", "#827345", "#9b76b8", "#482863"]
+const checkboxes = document.querySelectorAll('.filter-checkbox');
+
+function styleBy(field){
+    if (field < 150000) {
+    return ({ 
+    fill: true,
+    weight: 1,
+    fillColor: colors[0],
+    color: colors[0],
+    fillOpacity: 0.8,
+    opacity: 1,
+    });
+  } else if (field  >= 150000 && field < 300000) {
+    return ({ 
+    fill: true,
+    weight: 1,
+    fillColor: colors[1],
+    color: colors[1],
+    fillOpacity: 0.8,
+    opacity: 1,
+    }); 
+  } else if (field  >= 300000 && field  < 430000) {
+    return ({ 
+    fill: true,
+    weight: 1,
+    fillColor: colors[2],
+    color: colors[2],
+    fillOpacity: 0.8,
+    opacity: 1,
+    }); 
+  } else if (field  >= 430000) {
+    return ({ 
+    fill: true,
+    weight: 1,
+    fillColor: colors[3],
+    color: colors[3],
+    fillOpacity: 0.8,
+    opacity: 1,
+    }); 
+  } else {
+    return ({ 
+      fill: true,
+      weight: 1,
+      fillColor: colors[0],
+      color: colors[0],
+      fillOpacity: 0.8,
+      opacity: 1,
+      });
+  }
+};
+
+var display = 0;
+
+for (const checkbox of checkboxes){
+  checkbox.addEventListener('change', (evt) => {
+      if (evt.target.checked){
+          console.log('you clicked on the checkbox ' + checkbox.value);
+          if (checkbox.value == 1){
+            display = 1;
+          } else  if (checkbox.value == 2){
+            display = 2;
+          } else if (checkbox.value == 3){
+            display = 3;
+          } else {
+            display = 4
+          }
+      } else {
+          console.log('you unclicked the checkbox ' + checkbox.value);
+      }
+  });
+}
+      
 function initMap() {
     const map = L.map('map-container').setView([39.98, -75.2], 12);
   
@@ -9,58 +82,39 @@ function initMap() {
         attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     }).addTo(map);
     
-  const colors = ["#f0bc1f", "#827345", "#9b76b8", "#482863"]
+  
     //const tiles = downloadTiles();
 
 	const url = "https://storage.googleapis.com/musa509s23_team02_public/tiles/properties/{z}/{x}/{y}.pbf"
-  
-  //put dependencies on filter chexkbox checked DOM, not created yet
-  const vectorTileStyling = {
+
+  var vectorTileStyling = {
+        
     property_tile_info: (properties, zoom) => { 
+        
       var current_value = properties.current_assessed_value;
       var tax_year_value = properties.tax_year_assessed_value;
       var perc_change = ((current_value-tax_year_value)/tax_year_value)*100;
       var dollar_change = current_value-tax_year_value;
-      if (current_value < 150000) {
+      if (display==1){
+        return styleBy(current_value);
+      } else if (display==2){
+        return styleBy(tax_year_value);
+      } else if (display==3) {
+        return styleBy(perc_change);
+      } else if (display==4) {
+        return styleBy(dollar_change);
+      } else {
         return ({ 
-        fill: true,
-        weight: 1,
-        fillColor: colors[0],
-        color: colors[0],
-        fillOpacity: 0.8,
-        opacity: 1,
-        });
-      } 
-      if (current_value >= 150000 && current_value < 300000) {
-        return ({ 
-        fill: true,
-        weight: 1,
-        fillColor: colors[1],
-        color: colors[1],
-        fillOpacity: 0.8,
-        opacity: 1,
-        }); 
+          fill: true,
+          weight: 1,
+          fillColor: colors[0],
+          color: colors[0],
+          fillOpacity: 0.8,
+          opacity: 1,
+          });
       }
-      if (current_value >= 300000 && current_value < 430000) {
-        return ({ 
-        fill: true,
-        weight: 1,
-        fillColor: colors[2],
-        color: colors[2],
-        fillOpacity: 0.8,
-        opacity: 1,
-        }); 
-      }
-      if (current_value >= 430000) {
-        return ({ 
-        fill: true,
-        weight: 1,
-        fillColor: colors[3],
-        color: colors[3],
-        fillOpacity: 0.8,
-        opacity: 1,
-        }); 
-      }
+      
+      
     }
   };
 
