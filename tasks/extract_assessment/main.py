1352169e40c540df80c1fae7c54fe9d02079c2e7
zhaxinge <zhaxinge@upenn.edu>
2023-04-26 13:20:58 -0400

    opa assessment update

---
+++ tasks/extract_assessment/main.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from google.cloud import storage
+import functions_framework
+
+@functions_framework.http
+def prepare_assessments(request):
+    client = storage.Client()
+    bucket = client.bucket("musa509s23_team02_prepare_data")
+   
+    url = "https://phl.carto.com/api/v2/sql?format=geojson&skipfields=cartodb_id&q=SELECT%20*%20FROM%20assessments"
+    filename = 'assessments.csv'
+
+    response = urlopen(url)
+    blob = bucket.blob('opa_assessments/assessments.csv')
+    blob.upload_from_string(response.read(), content_type='application/csv')
+    
+    return 'OK'
+
+
+
+
+
+
--- tasks/prepare_opa_assessment.py
+++ tasks/prepare_opa_assessment.py
@@ -8,17 +8,15 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:\\Users\\DELL\\Documents\\Gith
 
 
 client = storage.Client()
-bucket = client.bucket("musa509s23_team02_prepared_data")
+bucket = client.bucket("musa509s23_team02_raw_data")
+raw_bucket =  client.bucket("musa509s23_team02_raw_data")
+processed_bucket =client.bucket("musa509s23_team02_prepared_data")
+raw_blob = raw_bucket.blob("opa_assessments/assessments.csv")
+processed_blob = processed_bucket.blob('opa_assessments/assessments.jsonl')
 
-blob = bucket.blob("opa_assessments/assessments.csv")
-# Download the CSV file as a string
-csv_data = blob.download_as_string().decode('utf-8')
-
-processed_blob = bucket.blob('opa_assessments/assessments.jsonl')
-# Convert CSV data to JSONL format
-jsonl_data = []
-for row in csv.DictReader(csv_data.splitlines()):
-    jsonl_data.append(json.dumps(row))
+content = raw_blob.download_as_string()
+data = csv.DictReader(content.decode('utf-8').splitlines())
+jsonl_data = [json.dumps(row) for row in data]
 
 processed_blob.upload_from_string('\n'.join(jsonl_data), content_type = 'application/jsonl')
 
