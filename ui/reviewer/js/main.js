74d9b759d2ada12b5e870043d3a933759fb9979f
Mjumbe Poe <mjumbewu@gmail.com>
2023-05-04 23:36:45 -0400

    Add a recent-value chart

--- ui/reviewer/js/main.js
+++ ui/reviewer/js/main.js
@@ -1,7 +1,10 @@
 import { initMap } from './map.js';
+import { showRecentValueChart } from './recent_value_chart.js';
 
 console.log("main");
 
 const map = initMap();
+const recentValueChart = showRecentValueChart();
 
 window.mapview = map;
+window.recentValueChart = recentValueChart;
